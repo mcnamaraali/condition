79e8d671c897ba32030d296144436bc4f16c18bb
alimcnamara <mcnamaraali@gmail.com>
2020-03-03 22:00:19 +0100

    Initial commit

---
+++ https:/github.com/mcnamaraali/condition/Conditions/src/Condition.java
@@ -0,0 +1,23 @@
+
+public class Condition {
+	public static void main(String[] args) {
+		
+		//postfix decrement operator 
+		
+		int c = 5; //declaring and assigning the variable c 
+		System.out.printf("c before postincrementation: %d%n", c);
+		System.out.printf("c postincrementation: %d%n", c--);
+		System.out.printf("c after postincrementation: %d%n", c);
+		
+		System.out.println();
+		
+		c = 5;
+		//prefix decrement operator
+		
+		System.out.printf("c before postincrementation: %d %n", c);
+		System.out.printf("c before postincrementation: %d %n", --c);
+		System.out.printf("c before postincrementation: %d %n", c);
+	
+	}
+
+}
